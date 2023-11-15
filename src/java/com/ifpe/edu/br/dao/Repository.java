5eaaa3ab26fa0179c4963b648db243e484da1077
Will <wjefferson.santos@gmail.com>
2023-11-15 12:45:11 -0300

    Implementa classe Repository
    Implenta classe que gerencia o armazenamento de objetos no bando de
    dados

--- src/java/com/ifpe/edu/br/dao/Repository.java
+++ src/java/com/ifpe/edu/br/dao/Repository.java
@@ -5,10 +5,63 @@
  */
 package com.ifpe.edu.br.dao;
 
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 /**
  *
  * @author wjeff
  */
 public class Repository {
-    
+    private static Repository instance;
+
+    public static Repository getInstance(){
+        if(instance == null)
+            instance = new Repository();
+        return instance;
+    }
+
+    private EntityManagerFactory emf = null;
+
+    private Repository(){
+        this.emf = Persistence.createEntityManagerFactory("TicDoguinhoPU");
+    }
+
+    public void insert(Object o){
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+        em.persist(o);
+        em.flush();
+        em.getTransaction().commit();
+        em.close();
+    }
+
+    public void update(Object o){
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+        em.merge(o);
+        em.getTransaction().commit();
+        em.close();
+    }
+
+    public List read(String query,Class c){
+        EntityManager em = emf.createEntityManager();
+        List returnedList = em.createQuery(query,c).getResultList();
+        em.close();
+        return returnedList;
+    }
+
+    public void delete(Object o){
+        EntityManager em = emf.createEntityManager();
+        Object oDelete = o;
+        if(!em.contains(o)){
+            oDelete = em.merge(o);
+        }
+        em.getTransaction().begin();
+        em.remove(oDelete);
+        em.getTransaction().commit();
+        em.close();
+    }
 }
